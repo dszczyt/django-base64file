6d85b56c6f1cbbec1efba92357956859b246d7b0
Damien SZCZYT <damien@pleinnet.fr>
2011-11-09 11:47:38 +0100

    guessing file type

--- base64file/utils.py
+++ base64file/utils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.core.files import File
-import base64
+import base64, mimetypes
 
 class Base64File(File):
     @classmethod
@@ -8,8 +8,9 @@ class Base64File(File):
         pass
 
     def base64(self):
+        mimetype = mimetypes.guess_type(self.name)[0] or "text/plain"
         return "data:%s;filename:%s;base64,%s" % (
-            "",
+            mimetype,
             self.name,
             base64.b64encode(self.read()),
         )
